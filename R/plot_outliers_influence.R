febb6209aa56e7d8e81a4cbb6cea52311ab2e8e1
Xiwen Feng <xiwenf@umich.edu>
2022-11-23 16:42:03 -0500

    cc

--- R/plot_outliers_influence.R
+++ R/plot_outliers_influence.R
@@ -13,36 +13,38 @@
 #'@export
 #'
 #plots DFFITS and labels the subjects that are outliers
-plotdffits <- function(lm.model){
-  p <- lm.model$rank
-  degree <- lm.model$rank + lm.model$df.residual
-  out <- 2*sqrt(p/degree)
-  x <- 1:degree
-  y <-dffits(lm.model)
-  plotdff <- data.frame(x, y)
-  colnames(plotdff) <- c("Observation", "DFFITS")
-  sub <- subset(plotdff, DFFITS>out | DFFITS<(-out))
-  ggplot(plotdff, aes(Observation, DFFITS, ymax = DFFITS , ymin = 0)) +
-    geom_linerange(color = "red") +
-    geom_hline(yintercept = out, color = "blue") +
-    geom_hline(yintercept = -out, color = "blue") +
-    labs(title = "Influence Diagnostics: DFFITS") +
-    geom_text(data = sub ,aes(label=Observation))
-}
-
-#plots Cook's Distance and labels the subjects that are outliers
-plotcd <- function(lm.model) {
-  p <- lm.model$rank
-  degree <- lm.model$rank + lm.model$df.residual
-  x <- 1:degree
-  cd <- cooks.distance(lm.model)
-  plotckd <- data.frame(x, cd)
-  colnames(plotckd) <- c("Observation", "CooksDist")
-  subcd <- subset(plotckd, CooksDist > 4/degree )
-  ggplot(plotckd, aes(Observation, CooksDist, ymax = CooksDist, ymin = 0)) +
-    geom_linerange(color = "red") +
-    geom_hline(yintercept = 4/degree , color = "blue") +
-    labs(title = "Influence Diagnostics: Cook's Distance") +
-    geom_text(data = subcd ,aes(label=Observation))
+plotoutliers<-function(lm.model,option= c("dffits", "cd")){
+  if (option == "dffits"){
+     p <- lm.model$rank
+     degree <- lm.model$rank + lm.model$df.residual
+     out <- 2*sqrt(p/degree)
+     x <- 1:degree
+     y <-dffits(lm.model)
+     plotdff <- data.frame(x, y)
+     colnames(plotdff) <- c("Observation", "DFFITS")
+     sub <- subset(plotdff, DFFITS>out | DFFITS<(-out))
+     ggplot(plotdff, aes(Observation, DFFITS, ymax = DFFITS , ymin = 0)) +
+       geom_linerange(color = "red") +
+       geom_hline(yintercept = out, color = "blue") +
+       geom_hline(yintercept = -out, color = "blue") +
+       labs(title = "Influence Diagnostics: DFFITS") +
+       geom_text(data = sub ,aes(label=Observation))
+   } else if ( option == "cd") {
+   #plots Cook's Distance and labels the subjects that are outliers
+     plotcd <- function(lm.model) {
+     p <- lm.model$rank
+     degree <- lm.model$rank + lm.model$df.residual
+     x <- 1:degree
+     cd <- cooks.distance(lm.model)
+     plotckd <- data.frame(x, cd)
+     colnames(plotckd) <- c("Observation", "CooksDist")
+     subcd <- subset(plotckd, CooksDist > 4/degree )
+     ggplot(plotckd, aes(Observation, CooksDist, ymax = CooksDist, ymin = 0)) +
+       geom_linerange(color = "red") +
+       geom_hline(yintercept = 4/degree , color = "blue") +
+       labs(title = "Influence Diagnostics: Cook's Distance") +
+       geom_text(data = subcd ,aes(label=Observation))
+     }
+   }
 }
 
