223f71fcb0697fc6ab9d6031abc0561ff6bbc2d9
Xiwen Feng <xiwenf@umich.edu>
2022-11-24 16:22:49 -0500

    delete plot

--- tests/testthat/test-plot_outliers_influence.R
+++
@@ -1,5 +0,0 @@
-test_that("outlierinfluence works", {
-  model <- lm(mpg~cyl + wt + qsec + disp, mtcars)
-  expect_equal(plotoutliers(model,option = "dffits"),plotoutliers(model,option= "dffits"))
-  expect_equal(plotoutliers(model,option = "cd"),plotoutliers(model,option= "cd"))
-  })
