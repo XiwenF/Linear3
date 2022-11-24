c04b816ffcf82c74ced17a8d81e7a88478d79f3c
Xiwen Feng <xiwenf@umich.edu>
2022-11-24 17:06:42 -0500

    delete rcpp

--- R/Linear3-paackage.R
+++
@@ -1,8 +0,0 @@
-## usethis namespace: start
-#' @useDynLib Linear3, .registration = TRUE
-## usethis namespace: end
-NULL
-## usethis namespace: start
-#' @importFrom Rcpp sourceCpp
-## usethis namespace: end
-NULL
